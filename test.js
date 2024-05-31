22bac8a7e80304d8c03b905f40a35ffbc4e222ab
Dimitris Thlivitis <dhmhtrhsxios101@gmail.com>
2024-05-31 11:34:46 +0000

    evala to swapi k douleuei

---
+++ test.js
@@ -0,0 +1,6 @@
+fetch("https://ci-swapi.herokuapp.com/api/").then(response => response.text()).then(data => displayData(data)).catch((e) => {
+    console.error("Error:", e);
+})
+function displayData(data) {
+    document.getElementById('content').innerText = data;
+}
